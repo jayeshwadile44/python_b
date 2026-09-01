8d4259240852dabcfa0b5e9177df37eee11751dc
Jayesh Wadile <jayeshwadile44@gmail.com>
2026-09-01 22:20:44 +0530

    lbreak_continue_pass

---
+++ 08_conditionals/continuous.py
@@ -0,0 +1,6 @@
+for i in range(1,20):
+    if i ==10:
+        continue # continue the loop for the next iteration here itself. ( Iteration means continue one loop and go to the next loop)
+    print (i)
+
+    
