ac31a21292cc065550707e7cc4fa3d43057333eb
Jayesh Wadile <jayeshwadile44@gmail.com>
2026-09-01 17:52:03 +0530

    match_case

---
+++ 08_conditionals/match_case.py
@@ -0,0 +1,13 @@
+# This was introduced in python 3.10
+
+a = int ( input ("Enter number between 1 and 10: "))
+
+match a:
+    case 1:
+        print("You won a lottery")
+    case 2:
+        print("You won a Charger")
+    case 3:
+        print("You won a gift")
+    case _:
+        print("Better Luck next time")
