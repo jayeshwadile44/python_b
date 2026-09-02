c06942c2817e02bd1ebf43c3c80ed23aa22f8420
Jayesh Wadile <jayeshw1@kpit.com>
2026-09-02 14:37:01 +0530

    practice_set_2

---
+++ python_basics_practice_set_2/question_1.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+print (num)
+
+if (num<0):
+    print("Number is Negative!")
+elif (num ==0):
+    print("Number is Positive!")
+else:
+    print("Number is 0")
---
+++ python_basics_practice_set_2/question_2.py
@@ -0,0 +1,6 @@
+age = int (input("Enter your age: "))
+
+if (age>=18):
+    print( "Congratulations! you are eligible to vote.")
+else:
+    print("Sorry! you are not eligible to vote.")
