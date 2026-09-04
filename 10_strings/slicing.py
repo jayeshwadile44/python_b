09642ae17c064a4ae93027a0a001776f470f331f
Jayesh Wadile <jayeshwadile44@gmail.com>
2026-09-04 19:24:34 +0530

    string_slicing_and_indexing

---
+++ 10_strings/slicing.py
@@ -0,0 +1,20 @@
+name = "Jayesh"
+
+# J   a   y   e   s   h  #total is 6.
+# 0   1   2   3   4   5
+#-6  -5  -4  -3  -2  -1 
+
+# print (name[0:3])  # prints the first three characters of the string
+
+# print(name[2:-1]) #same as [2:5]
+# print(name[2:5]) #prints from 2 to 4
+
+#print(name[0:10:n]) #skip n-1 characters
+print(name[0:10:1]) #skip 0 characters
+print(name[0:10:2]) #take one character, skip the next 2, take the next one with loop.-->Jys
+print(name[0:10:3]) #take one character, skip the next 3, take the next one with loop.-->Je
+
+print(name[:4]) #till 4 only.
+print(name[1:]) # after 1 all.
+print(name[2:]) # after 2 all.
+print(name[:2]) # till 2 only. 
